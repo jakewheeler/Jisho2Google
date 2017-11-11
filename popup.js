0dbf72ec2bf4d49e67b7cc43e2aeae5f3aede9cc
jakewheeler <jakezro00@aim.com>
2017-11-11 01:33:15 -0500

    Added css, removed unneeded code, and added some error handling. Need to fix japanese words not being searched correctly.

--- popup.js
+++ popup.js
@@ -15,47 +15,74 @@ function getJishoObject(word)
 
 function parseJishoObject(jishoObj)
 {
-  var word = jishoObj.data[0].japanese[0].word; // word
-  var reading = jishoObj.data[0].japanese[0].reading; // reading
-  var definitionArray = ""; // all definitions with part of speech
+  try
+  {
+    var word = jishoObj.data[0].japanese[0].word; // word
+    var reading = jishoObj.data[0].japanese[0].reading; // reading
+    var definitionArray = ""; // all definitions with part of speech
 
-  for (var i = 0; i < jishoObj.data[0].senses.length; i++) {
-    var partOfSpeech = jishoObj.data[0].senses[i].parts_of_speech[0];
+    for (var i = 0; i < jishoObj.data[0].senses.length; i++) {
+      var partOfSpeech = jishoObj.data[0].senses[i].parts_of_speech[0];
 
-    if (typeof partOfSpeech !== "undefined") {
-          definitionArray += "(" + partOfSpeech + ")" + " ";
+      if (typeof partOfSpeech !== "undefined") {
+            definitionArray += "(" + partOfSpeech + ")" + " ";
+      }
+
+      definitionArray += (i + 1) + ". ";
+      for (var j = 0; j < jishoObj.data[0].senses[i].english_definitions.length; j++) {
+
+            definitionArray += jishoObj.data[0].senses[i].english_definitions[j] + "; ";
+      }
     }
+    
+    var customObject = {word: word, reading: reading, definitions: definitionArray};
+    console.log(customObject);
 
-    definitionArray += (i + 1) + ". ";
-    for (var j = 0; j < jishoObj.data[0].senses[i].english_definitions.length; j++) {
+    return customObject;
+  }catch(err){
 
-          definitionArray += jishoObj.data[0].senses[i].english_definitions[j] + "; ";
-    }
+    setToError();
+    return null;
   }
-  
-  var customObject = {word: word, reading: reading, definitions: definitionArray};
 
-  return customObject;
   
+}
+
+function clearAllElementsData()
+{
+  $('#word').val("");
+  $('#reading').val("");
+  $('#definitions').val("");
+}
+
+function setToSuccess()
+{
+  $('#successLabel').css("color", "green");
+  $('#successLabel').html("Success");
+  $('#successStatus').show();
+  fadeSuccessStatus();
+}
+
+function setToError()
+{
+  $('#successLabel').css("color", "red");
+  $('#successLabel').html("Error updating spreadsheet");
+  $('#successStatus').show();
+  fadeSuccessStatus();
+}
+
+function fadeSuccessStatus()
+{
+  $('#successStatus').fadeOut(3000);
 }
 
 function fillInElementsWithData(parsedObject) {
-  $('#word').val(parsedObject.word);
-  $('#reading').val(parsedObject.reading);
-  $('#definitions').val(parsedObject.definitions);
-}
-
-function validEmail(email) { // see:
-  var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-  return re.test(email);
-}
-
-function validateHuman(honeypot) {
-  if (honeypot) {  //if hidden form filled up
-    console.log("Robot Detected!");
-    return true;
-  } else {
-    console.log("Welcome Human!");
+  try {
+      $('#word').val(parsedObject.word);
+      $('#reading').val(parsedObject.reading);
+      $('#definitions').val(parsedObject.definitions);
+  } catch(err) {
+    setToError();
   }
 }
 
@@ -101,60 +128,10 @@ function getFormData() {
   data.formGoogleSheetName = form.dataset.sheet || "vocabulary"; // default sheet name
   data.formGoogleSendEmail = form.dataset.email || ""; // no email by default
 
-  console.log(data);
+  //console.log(data);
   return data;
 }
 
-function handleFormSubmit(event) {  // handles form submit withtout any jquery
-    // get word from box
-    var wordEntered = $('#word').val();
-
-    // get all data from API
-    var allData = getJishoObject(wordEntered);
-    
-    var parsedObject = parseJishoObject(allData);
-    
-    fillInElementsWithData(parsedObject);
-
-  event.preventDefault();           // we are submitting via xhr below
-  var data = getFormData();         // get the values submitted in the form
-
-  /* OPTION: Remove this comment to enable SPAM prevention, see README.md
-  if (validateHuman(data.honeypot)) {  //if form is filled, form will not be submitted
-    return false;
-  }
-  */
-d
-  if( !validEmail(data.email) ) {   // if email is not valid show error
-    document.getElementById('email-invalid').style.display = 'block';
-    return false;
-  } else {
-    var url = event.target.action;  //
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', url);
-    // xhr.withCredentials = true;
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.onreadystatechange = function() {
-        console.log( xhr.status, xhr.statusText )
-        console.log(xhr.responseText);
-        document.getElementById('gform').style.display = 'none'; // hide form
-        document.getElementById('thankyou_message').style.display = 'block';
-        return;
-    };
-    // url encode form data for sending as post data
-    var encoded = Object.keys(data).map(function(k) {
-        return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
-    }).join('&')
-    xhr.send(encoded);
-  }
-}
-function loaded() {
-  console.log('contact form submission handler loaded successfully');
-  // bind to the submit event of our form
-  var form = document.getElementById('gform');
-  form.addEventListener("submit", handleFormSubmit, false);
-};
-document.addEventListener('DOMContentLoaded', loaded, false);
 document.addEventListener("DOMContentLoaded", function()
 {
   var subButton = document.getElementById('submitButton');
@@ -163,29 +140,38 @@ document.addEventListener("DOMContentLoaded", function()
      // get word from box
     var wordEntered = $('#word').val();
 
-    // get all data from API
-    var allData = getJishoObject(wordEntered);
-    
-    var parsedObject = parseJishoObject(allData);
-    
-    fillInElementsWithData(parsedObject);
+    if (wordEntered)
+    {
+      // get all data from API
+      var allData = getJishoObject(wordEntered);
+      
+      var parsedObject = parseJishoObject(allData);
+      
+      fillInElementsWithData(parsedObject);
 
-  //event.preventDefault();           // we are submitting via xhr below
-  var data = getFormData();         // get the values submitted in the form
+      var data = getFormData();
 
-    //var url = event.target.action;  //
-    var url = "https://script.google.com/macros/s/AKfycbw_X_Im0yb3UpOqutJ8XXwldjZjSm2GuNjuJMa7_zBnYCle5r6H/exec";
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', url);
-    // xhr.withCredentials = true;
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      if (data !== null && data.word && data.reading && data.definitions) {
+        var url = "https://script.google.com/macros/s/AKfycbw_X_Im0yb3UpOqutJ8XXwldjZjSm2GuNjuJMa7_zBnYCle5r6H/exec";
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', url);
+        // xhr.withCredentials = true;
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-    // url encode form data for sending as post data
-    var encoded = Object.keys(data).map(function(k) {
-        return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
-    }).join('&')
-    xhr.send(encoded);
+        // url encode form data for sending as post data
+        var encoded = Object.keys(data).map(function(k) {
+            return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+        }).join('&')
+        xhr.send(encoded);
 
+        setToSuccess();
+      }
+      else {
+        setToError();
+      }
+    }
+    else {
+      setToError();
+    }
 })
-
 });
